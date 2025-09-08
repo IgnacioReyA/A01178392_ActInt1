fdcc4e2c10cf1dcdb6f48aba4222b5f844d30f02
IgnacioReyA <ignacio.reyarslangul@gmail.com>
2025-09-08 16:13:05 -0600

    Agrega llamada de funcion subsecuencias al main

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 #include "./Codigos/huffman.h"
 #include "./Codigos/palindromo.h"
 #include "./Codigos/subsecuencias.h"
@@ -6,7 +8,46 @@
 
 using namespace std;
 
+string leerArchivo(const string& nombreArchivo) {
+    ifstream archivo(nombreArchivo);
+    string contenido;
+    string linea;
+    
+    if (archivo.is_open()) {
+        while (getline(archivo, linea)) {
+            contenido += linea;
+        }
+        archivo.close();
+    } else {
+        cout << "Error: No se pudo abrir el archivo " << nombreArchivo << endl;
+    }
+    
+    return contenido;
+}
+
 int main() {
-    cout << "Hello, World!" << endl;
+    cout << "=== Parte 1 - Búsqueda de subsecuencias ===" << endl;
+    
+    // Leer archivos de transmisión
+    string transmission1 = leerArchivo("Archivos Entrada/transmission1.txt");
+    string transmission2 = leerArchivo("Archivos Entrada/transmission2.txt");
+    
+    // Leer archivos de códigos maliciosos
+    string mcode1 = leerArchivo("Archivos Entrada/mcode1.txt");
+    string mcode2 = leerArchivo("Archivos Entrada/mcode2.txt");
+    string mcode3 = leerArchivo("Archivos Entrada/mcode3.txt");
+    
+    // Buscar subsecuencias en transmission1
+    cout << "\nTransmission1.txt:" << endl;
+    cout << "mcode1: " << subsecuencias(transmission1, mcode1) << endl;
+    cout << "mcode2: " << subsecuencias(transmission1, mcode2) << endl;
+    cout << "mcode3: " << subsecuencias(transmission1, mcode3) << endl;
+    
+    // Buscar subsecuencias en transmission2
+    cout << "\nTransmission2.txt:" << endl;
+    cout << "mcode1: " << subsecuencias(transmission2, mcode1) << endl;
+    cout << "mcode2: " << subsecuencias(transmission2, mcode2) << endl;
+    cout << "mcode3: " << subsecuencias(transmission2, mcode3) << endl;
+    
     return 0;
 }
